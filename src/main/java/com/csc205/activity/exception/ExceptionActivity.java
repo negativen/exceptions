1d22db5cfa80baf364b9590da928826f8618e7cd
negativen <wilsnat94@gmail.com>
2022-09-22 19:22:02 -0700

    ExceptionActivity

--- src/main/java/com/csc205/activity/exception/ExceptionActivity.java
+++ src/main/java/com/csc205/activity/exception/ExceptionActivity.java
@@ -1,2 +1,64 @@
-package com.csc205.activity.exception;public class ExceptionActivity {
+package com.csc205.activity.exception;
+
+import java.util.Scanner;
+
+public class ExceptionActivity {
+    public static void main(String[] args) {
+    //try-catch
+        try {
+            // happy path - all this executes if no exception thrown
+            int a[] = new int[2];
+            System.out.println("Access element three : " + a[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // sad path - exception thrown
+            System.out.println("Exception thrown : " + e);
+            System.out.println("Out of the block");
+            e.printStackTrace();
+        }
+
+        // try-catch-catch - order of catch blocks matters per hierarchy
+        try {
+            // Do something here
+        } catch (NullPointerException e) {
+            // we'll catch the NullPointerException here and handle it differently than other exceptions
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // do something
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            // always do something
+        }
+
+        try {
+            // do something we don't expect to throw exceptions
+        }
+        finally {
+            // but if we do, always execute code here
+        }
+
+        // try-with-resources
+        try(Scanner scanner1 = new Scanner(System.in)){
+            // do something with scanner1
+        }
+        Scanner scanner2 = null;
+        try {
+            scanner2 = new Scanner(System.in);
+        }
+        finally {
+            if (scanner2 != null) {
+                scanner2.close();
+            }
+        }
+
+        try (Scanner scanner3 = new Scanner(System.in)) {
+            // do something with scanner3
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
